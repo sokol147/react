91ed4a2a2ca9b630415a4afde6b5656e72f6e908
Sokol V <1470715@gmail.com>
2018-04-10 21:46:17 +0200

    table sort & redact

--- source/js/app.js
+++ source/js/app.js
@@ -95,6 +95,125 @@ let logMixin = {
 	},
 }
 
+
+
+
+let header = ['Book', 'Author', 'Language', 'Published', 'Sales'];
+let data = [
+  ['The Lord of the Rings', 'J. R. R. Tolkien', 'English', '1954-1955', '150 million'],
+  ['Le Petit Prince (The Little Peince)','Antoine de Saint-Exupery','French','1943','140 million'],
+  ['Harry Potter and the Philosopher Stone','J. K. Rowling','English','1977','107 million'],
+  ['And Than There Were None','Agatha Christie','English','1939','100 million'],
+  ['Dream of the Red Chamber','Cao Xueqin','Chinese','1754-1791','100 million'],
+  ['The Hobbit','J. R. R. Tolkien','English','1937','100 million'],
+  ['She: A History of Adventure','H. Rider Haggard','English','1877','100 million']
+]
+
+let Excel = React.createClass({
+	displayName: 'Excel',
+	propTypes: {
+		header: React.PropTypes.arrayOf(
+      React.PropTypes.string
+		),
+		initialData: React.PropTypes.arrayOf(
+      React.PropTypes.arrayOf(
+        React.PropTypes.string
+      )
+		),
+	},
+	_sort: function(e){
+		let column = e.target.cellIndex;
+		let data = this.state.data.slice();
+		let descending = this.state.sortby === column && !this.state.descending;
+		data.sort(function(a,b){
+			return descending
+				? (a[column] < b[column] ? 1 : -1) : (a[column] > b[column] ? 1 : -1);
+		});
+		this.setState({
+			data: data,
+			sortby: column,
+			descending: descending,
+		});
+		console.log('sort complite');
+	},
+	_showEditor: function(e){
+		this.setState({
+			edit: {
+				row: parseInt(e.target.dataset.row, 10),
+				cell: e.target.callIndex,
+			}
+		});
+		console.log('open editor');
+	},
+	_save: function(e){
+		e.preventDdfault();
+		let input = e.target.firstChild;
+		let data = this.state.data.slice();
+		state: data[this.state.edit.row][this.state.edit.cell] = input.value;
+		this.setState({
+			edit: null,
+			data: data,
+		})
+	},
+	getInitialState: function(){
+		return {
+			data: this.props.initialData,
+			sortby: null,
+			descending: false,
+			edit: null,
+		};
+	},
+	render: function(){
+		return (
+			React.DOM.table({className: 'compare_table'},
+				React.DOM.thead({onClick: this._sort},
+					React.DOM.tr(null,
+						this.props.header.map(function(title, index){
+							if (this.state.sortby === index){
+								title += this.state.descending ? ' \u2191' : ' \u2193'
+							}
+							return React.DOM.th({key: index}, title);
+						}, this)
+					)
+				),
+				React.DOM.tbody({onDoubleClick: this._showEditor},
+					this.state.data.map(function(row, rowidx){
+            return (
+              React.DOM.tr({key: rowidx},
+                row.map(function(cell, index){
+                  let content = cell;
+                  let edit = this.state.edit;
+                  if(edit && edit.row === rowidx && edit.cell === index){
+                    content = React.DOM.form({onSubmit: this._save},
+                      React.DOM.input({
+                        type: 'text',
+                        dafaultValue: content,
+                      })
+                    );
+                  }
+                  return React.DOM.td({
+                    key: index,
+                    'data-row': rowidx
+                  }, content);
+                }, this)
+              )
+            );
+					}, this)
+				)
+			)
+		)
+	}
+});
+
+ReactDOM.render(
+	React.createElement(Excel,{
+		header: header,
+		initialData: data,
+	}),
+	document.getElementById('two')
+)
+
+/*
 let Counter = React.createClass({
 	name: 'Counter',
 	// mixins: [logMixin],
@@ -158,4 +277,4 @@ ReactDOM.render(
 		text: 'Vlad'
 	}),
 	document.getElementById('one')
-)
+)*/
